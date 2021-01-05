0f691579a025b3aeaa14b26f30d0fa33f7195b31
craig dunlap <cadunlap3234@gmail.com>
2021-01-05 10:57:29 -0500

    Add project files.

---
+++ Interface/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Stay();
+            dog.Sit();
+            dog.RollOver();
+            dog.Attack();
+
+            Console.WriteLine("Tell us your dog's name.");
+            dog.DogName = Console.ReadLine();
+            Console.WriteLine($"The dog is {dog.DogName}");
+
+            Console.WriteLine("What Breed of wolf is it?");
+            dog.WolfBreed = Console.ReadLine();
+            Console.WriteLine($"{dog.DogName} is {dog.WolfBreed}");
+
+            dog.Train();
+            Console.ReadKey();
+        }
+
+        //contain definitons for a group of related functionalities that a class or struct can impliment.
+
+        //Interfaces are a contract between structs and class.
+        //The interface defines the What in the contract.
+        //Classes and the struct define the how.
+    }
+
+    interface ITrainer
+    {
+        void Train();
+    }
+        interface IAnimalCommands
+        {
+        void Stay();
+        void Sit();
+        void RollOver();
+        void Attack();
+        string DogName { get; set; }
+        }
+
+
+        public class Dog : Wolf, IAnimalCommands, ITrainer
+        {
+        public string DogName { get; set; }
+        public void Stay()
+        {
+            Console.WriteLine("The Dog is Staying");
+        }
+        public void Sit()
+        {
+            Console.WriteLine("The Dog is Sitting");
+        }
+        public void RollOver()
+        {
+            Console.WriteLine("The Dog is rolling over");
+        }
+        public void Attack()
+        {
+            Console.WriteLine("The Dog is attacking");
+        }
+        public void Train()
+        {
+            Console.WriteLine($"the trainer is training {DogName}");
+        }
+    }
+
+    public class Wolf
+    {
+        public string WolfBreed { get; set; }
+    }
+}
---
+++ TrainingVideo/Interfaces.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingVideo
+{
+    class Interfaces
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        //An interface contains definitions for a group of related functionalities that a class or struct can implement.
+
+
+        //Think of interfaces as a contract with our classes and structs. 
+        //The Interface defines what will happen.
+        //The Classes and Structs will define how it happens.
+
+
+    }
+}
